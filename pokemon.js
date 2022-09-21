f753c8fa72ba0c1a9df8adbdb0aee8e670bd346b
fsaiz97 <felixsaiz97@gmail.com>
2022-09-21 12:10:59 +0100

    Add "next button" functionality

--- pokemon.js
+++ pokemon.js
@@ -2,28 +2,43 @@ function capitalize(str) {
     return str[0].toUpperCase() + str.slice(1);
 }
 
-const fetchPokemon = async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
-    const pokemonData = await response.json();
-
-    // Pokemon name
-    let pokemonName = document.querySelector("#pokemonName");
-    pokemonName.textContent = capitalize(pokemonData.name);
-    // or pokemonName.style.textTransform = 'capitalize';
-
-    // Pokemon sprites
-    const pokemonImageFront = document.querySelector("#pokeImageFront");
-    pokemonImageFront.src = pokemonData.sprites.front_default;
-    const pokemonImageBack = document.querySelector("#pokeImageBack");
-    pokemonImageBack.src = pokemonData.sprites.back_default;
-
-    // Pokemon moves
-    const movesList = document.querySelector("#moves");
-    for(let i = 0; i < 5; i++) {
-        let moveItem = document.createElement('li');
-        moveItem.textContent = pokemonData.moves[i].move.name;
-        movesList.append(moveItem);
+const fetchPokemon = async (pokemonIndex) => {
+    try {
+        let pokemonURL = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`
+        const response = await fetch(pokemonURL);
+        const pokemonData = await response.json();
+    
+        // Pokemon name
+        let pokemonName = document.querySelector("#pokemonName");
+        pokemonName.textContent = capitalize(pokemonData.name);
+        // or pokemonName.style.textTransform = 'capitalize';
+    
+        // Pokemon sprites
+        const pokemonImageFront = document.querySelector("#pokeImageFront");
+        pokemonImageFront.src = pokemonData.sprites.front_default;
+        const pokemonImageBack = document.querySelector("#pokeImageBack");
+        pokemonImageBack.src = pokemonData.sprites.back_default;
+    
+        // Pokemon moves
+        const movesList = document.querySelector("#moves");
+        // clear current moves
+        movesList.innerHTML = "";
+        for(let i = 0; i < 5; i++) {
+            let moveItem = document.createElement('li');
+            moveItem.textContent = pokemonData.moves[i].move.name;
+            movesList.append(moveItem);
+        }
+    } catch (err) {
+        console.log(err)
     }
 }
 
-fetchPokemon();
+let index = 1;
+
+fetchPokemon(index);
+
+const nextButton = document.querySelector('#nextPokemon');
+nextButton.addEventListener('click', () => {
+    index++;
+    fetchPokemon(index);
+})
