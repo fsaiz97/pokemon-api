b99973929c114d8847c55378440624e085e806d0
fsaiz97 <felixsaiz97@gmail.com>
2022-09-21 12:58:40 +0100

    - Refactor fetchPokemon function

--- pokemon.js
+++ pokemon.js
@@ -2,31 +2,25 @@ function capitalize(str) {
     return str[0].toUpperCase() + str.slice(1);
 }
 
-const fetchPokemon = async (searchTerm) => {
-    try {
-        let pokemonURL = `https://pokeapi.co/api/v2/pokemon/${searchTerm}`;
-        const response = await fetch(pokemonURL);
-        const pokemonData = await response.json();
-    
-        // Pokemon name
-        let pokemonName = document.querySelector("#pokemonName");
-        pokemonName.textContent = capitalize(pokemonData.name);
-        // or pokemonName.style.textTransform = 'capitalize';
-    
-        // Pokemon sprites
-        const pokemonImageFront = document.querySelector("#pokeImageFront");
-        pokemonImageFront.src = pokemonData.sprites.front_default;
-        const pokemonImageBack = document.querySelector("#pokeImageBack");
-        pokemonImageBack.src = pokemonData.sprites.back_default;
-    
-        // Pokemon moves
-        const movesList = document.querySelector("#moves");
+function getPokemonName(data) {
+    let pokemonName = document.querySelector("#pokemonName");
+    pokemonName.textContent = capitalize(data.name);
+    // or pokemonName.style.textTransform = 'capitalize';
+}
+
+function getPokemonImage (imageType, data) {
+    const image = document.querySelector(`#pokeImage${capitalize(imageType)}`);
+    image.src = data.sprites[`${imageType}_default`];
+}
+
+function getPokemonMoves(data) {
+    const movesList = document.querySelector("#moves");
         // clear current moves
         movesList.innerHTML = "";
         for(let i = 0; i < 5; i++) {
             try {
                 let moveItem = document.createElement('li');
-                moveItem.textContent = pokemonData.moves[i].move.name;
+                moveItem.textContent = data.moves[i].move.name;
                 movesList.append(moveItem);
             } catch (err) {
                 if (err.name === "TypeError") {
@@ -36,6 +30,23 @@ const fetchPokemon = async (searchTerm) => {
                 }
             }
         }
+}
+
+const fetchPokemon = async (searchTerm) => {
+    try {
+        let pokemonURL = `https://pokeapi.co/api/v2/pokemon/${searchTerm}`;
+        const response = await fetch(pokemonURL);
+        const pokemonData = await response.json();
+
+        // Pokemon name
+        getPokemonName(pokemonData);
+    
+        // Pokemon sprites
+        getPokemonImage ("front", pokemonData);
+        getPokemonImage ("back", pokemonData);
+
+        // Pokemon moves
+        getPokemonMoves(pokemonData);
     } catch (err) {
         console.log(err)
     }
