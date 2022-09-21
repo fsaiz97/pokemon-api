5f31814a0df9c38992e62eb81e16afd3de966b16
fsaiz97 <felixsaiz97@gmail.com>
2022-09-21 12:00:02 +0100

    Display basic data from the hardcoded API request

--- pokemon.js
+++ pokemon.js
@@ -1,7 +1,29 @@
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
 const fetchPokemon = async () => {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
     const pokemonData = await response.json();
-    console.log(pokemonData);
+
+    // Pokemon name
+    let pokemonName = document.querySelector("#pokemonName");
+    pokemonName.textContent = capitalize(pokemonData.name);
+    // or pokemonName.style.textTransform = 'capitalize';
+
+    // Pokemon sprites
+    const pokemonImageFront = document.querySelector("#pokeImageFront");
+    pokemonImageFront.src = pokemonData.sprites.front_default;
+    const pokemonImageBack = document.querySelector("#pokeImageBack");
+    pokemonImageBack.src = pokemonData.sprites.back_default;
+
+    // Pokemon moves
+    const movesList = document.querySelector("#moves");
+    for(let i = 0; i < 5; i++) {
+        let moveItem = document.createElement('li');
+        moveItem.textContent = pokemonData.moves[i].move.name;
+        movesList.append(moveItem);
+    }
 }
 
-fetchPokemon()
+fetchPokemon();
